2eb0e532be5e367d39aa16cddc1047c8de6cc02d
rszktr <reszketo.robert@gmail.com>
2022-09-07 20:48:39 +0200

    solution

--- 13-promise/exercises/01/solution/js/convertToUppercase.js
+++ 13-promise/exercises/01/solution/js/convertToUppercase.js
@@ -1,1 +1,11 @@
+const convertToUppercase = (array) => {
+    return new Promise((resolve, reject) => {
+        if (array.every(item => typeof item == 'string')) {
+            resolve(array.map(item2 => item2.toLocaleUpperCase()))
+        } else {
+            reject(new Error('Error: Not all items in the array are strings!'))
+        }
+    })
+}
+
 export default convertToUppercase;
--- 13-promise/exercises/02/solution/js/sortStrings.js
+++ 13-promise/exercises/02/solution/js/sortStrings.js
@@ -1,1 +1,23 @@
+const sortStrings = (array) => {
+    return new Promise((resolve, reject) => {
+        try {
+            array.every(item => typeof item === 'string')
+        }
+        catch (error) {
+            reject(new Error('Error: Something went wrong with sorting words!'))
+        }
+        if (array.every(item => typeof item === 'string')) {
+            try {
+                array.sort();
+            } catch (error) {
+                reject(new Error('Error: Something went wrong with sorting words!'));
+            }
+        } else {
+            reject(new Error('Error: Not all items in the array are strings!'))
+        };
+        resolve(array.sort())
+    })
+}
+
+
 export default sortStrings;
